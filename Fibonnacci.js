98da2593fe107297f706605d1edc77b72ff325b1
SalauddinAmir <mdsalauddinahmed.amir@gmail.com>
2023-02-03 18:25:46 +0600

    fibonnaccii seris

---
+++ Fibonnacci.js
@@ -0,0 +1,33 @@
+//  function fibo(num){
+//     let seris =[];
+//     for(let i=num;i>=num;i--){
+//         let element=num[i];
+//         element=num[i-1]+num[i-2];
+//         seris.push(element);
+//     }
+//     return seris;
+//  }
+//  const fibonacci=fibo(50)
+//  console.log(fibonacci)
+const fibo =[0,1]
+for(let i=2;i<=30;i++){
+    fibo[i]=fibo[i-1]+fibo[i-2];
+}
+console.log(fibo)
+
+// function
+const fibonacci = (n) => {
+    if(n <= 1) {
+      return n;
+    }
+  
+    const result = [0, 1];
+  
+    for (let i = 2; i <= n; i++) {
+      result[i] = result[i - 2] + result[i - 1];
+    }
+  
+    return result;
+  }
+  const fibonaci =fibonacci(6)
+  console.log(fibonaci)
